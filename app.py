6c5a76489d65303b3dde3fb5082d097e41c82efb
Wondrous27 <berk.guler@windowslive.com>
2020-05-16 17:38:30 +0300

    updates

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_session import Session
+import psycopg2
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -11,6 +13,10 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+app.config["SESSION PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+notes =[]
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
@@ -31,7 +37,19 @@ def internal_server_error(e):
 def index():
     name = None
     form = NameForm()
+    if session.get("notes") is None:
+        session["notes"] = []
+
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+    creators = ["Batuhan", "Berk", "Bombar", "Enver"]
+    if request.method == "POST":
+        note = request.form.get("note")
+        session["notes"].append(note)
+    return render_template('index.html', form=form, name=name, creators = creators, notes = session["notes"])
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name= name)
+
